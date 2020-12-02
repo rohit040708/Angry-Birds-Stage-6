b7fa821088bf360e37c6ab2f70621edb9cc1af53
samar <67219193+rohit040708@users.noreply.github.com>
2020-12-02 14:05:11 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    if(this.body.velocity.x>5 && this.body.position.x>200){
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+    }
+    for (var I = 0; I<this.trajectory.length; I++){
+      image(this.smokeimage, this.trajectory[I][0], this.trajectory[I][1]);
+
+    }
+  }
+}
